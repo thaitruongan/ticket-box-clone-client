32c5818959dbe9adc77fbb3d8807d875237a91ad
trinhthanhtung1010 <trinhthanhtung1010@gmail.com>
2021-12-06 17:40:00 +0700

    sign in page

--- ticket-box-client/src/App.js
+++ ticket-box-client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import ImportOtp from './features/sign-in/importOTP/ImportOtp';
+import SignIn from './features/sign-in/SignIn';
+import SignUp from './features/sign-up/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <h1>Start</h1>
+      <SignIn/>
     </div>
   );
 }
